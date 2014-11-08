217f75b426c27607097dc07914b5db6de9373136
Jack Donovan <Jyakku@gmail.com>
2014-11-08 00:43:02 -0500

    Additional project files as and skeleton code for planet manager.

---
+++ Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlanetManager : MonoBehaviour 
+{
+	List<GameObject> allPlanets;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//initialize our list
+		allPlanets = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void FindAllPlanets()
+	{
+		GameObject[] planetArray = GameObject.FindGameObjectsWithTag ("Planet");
+
+		for(int i = 0; i < planetArray.Length; i++)
+		{
+			allPlanets.Add(planetArray[i]);
+		}
+	}
+}
---
+++ Assets/Scripts/ScreenSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSystem : MonoBehaviour 
+{
+	Resolution nativeResolution;
+	bool isCorrected = false;
+	bool useCorrectedResolution = true; //should always be true unless currently testing
+
+	// Use this for initialization
+	void Start () 
+	{
+		nativeResolution = Screen.resolutions [Screen.resolutions.Length - 1];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(isCorrected || !useCorrectedResolution)
+			return;
+
+		if(nativeResolution.width != Screen.width || nativeResolution.height != Screen.height)
+		{
+			Screen.SetResolution(nativeResolution.width, nativeResolution.height, true);
+			isCorrected = true;
+		}
+	}
+}
