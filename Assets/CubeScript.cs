18fa589ee3ebe3ca96e632e153919c8877ba71e2
Jack Donovan <Jyakku@gmail.com>
2014-11-07 23:24:17 -0500

    Initial commit.

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		gameObject.transform.Translate (Vector3.up);
+	}
+
+	void OnCollisionEnter()
+	{
+
+
+	}
+
+	void OnCollisionStay()
+	{
+
+	}
+}
