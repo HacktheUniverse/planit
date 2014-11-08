a39549bce628dfb0f27194e4cba983fe5d4fb94b
IT <it@look.local>
2014-11-08 02:58:46 -0500

    Physics fixes to bam.cs.

--- example-code/bam.cs
+++ example-code/bam.cs
@@ -5,21 +5,24 @@ using System.Collections;
 
 public class bam : MonoBehaviour {
 
+	const int n = 20;
 	GameObject[] pls; 
-	float G = 0.001f;
+	float G = 0.01f;
 	float dt = 0.01f;
+	Vector3[] F;
 
 	// Use this for initialization
 	void Start () {	
 		Debug.Log ("bam!");
-		pls = new GameObject [10];
-		for (int i = 0; i < 10; i++) {	
+		pls = new GameObject [n];
+		F = new Vector3[n];
+		for (int i = 0; i < n; i++) {	
 			pls [i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			pls [i].AddComponent<Rigidbody> ();	
 			pls [i].rigidbody.useGravity = false;
-			pls [i].transform.position = Random.insideUnitSphere; // new Vector3 (0, 0, i);
+			pls [i].transform.position = Random.insideUnitSphere*10.0f;
 			pls [i].rigidbody.velocity = Vector3.zero; // Random.insideUnitSphere/10.0f;
-			pls[i].rigidbody.mass = 100.0f;
+			pls [i].rigidbody.mass = 100.0f;
 		}
 	}
 
@@ -30,20 +33,27 @@ public class bam : MonoBehaviour {
 		return  -G *
 				p.rigidbody.mass *	
 				q.rigidbody.mass *
-				1/Mathf.Pow(Vector3.Dot ((rp-rq),(rp-rq)),1.5f) * (rp-rq);
+				1/Mathf.Pow((rp-rq).sqrMagnitude,1.5f) * (rp-rq);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 F;
-		foreach ( GameObject p in pls ) {
-			foreach ( GameObject q in pls) {
-				F = Vector3.zero;
-				if (q != p) {
-					F = F + newton(p,q);
+		float m;
+		// calculate net force on each body
+		for (int i = 0; i < n; i++) {
+			F [i] = Vector3.zero;
+			for (int j = 0; j < n; j++) {
+				if (i != j) {
+					F [i] = F [i] + newton (pls [i], pls [j]);
+				}
 			}
-			p.transform.position = p.transform.position + p.rigidbody.velocity * dt + F / p.rigidbody.mass * dt*dt/2.0f;
+		}
+		// update velocities and positions for each body
+		for (int i = 0; i < n; i++) {
+			m = pls[i].rigidbody.mass;
+			pls[i].rigidbody.velocity = pls[i].rigidbody.velocity + F[i]/m*dt;
+			pls[i].transform.position = pls[i].transform.position + pls[i].rigidbody.velocity * dt + F[i] / m*dt*dt/2.0f;
 		}
 	}
 }
-}
+
